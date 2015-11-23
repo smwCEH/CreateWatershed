08ad9e53ab31a9e2c74b8281cf0aabff726e0f7e
Wright, Simon M <smw@ceh.ac.uk>
2015-11-23 15:41:49 +0000

    Uncommented watershed command

--- Watershed.py
+++ Watershed.py
@@ -7,23 +7,23 @@ import arcpy
 arcpy.env.overwriteOutput = True
 
 
-# # Generate watersheds using AGOL Watersheds hydrology geoprocessing service
-# arcpy.ImportToolbox('http://hydro.arcgis.com/arcgis/services;Tools/Hydrology;<username>;<password>', 'hydro')
-# result = arcpy.Watershed_hydro(r'F:\DonMonteith\Watersheds\AWMN.shp',
-#                                'FID',
-#                                '',
-#                                'Meters',
-#                                'FINEST',
-#                                'False',
-#                                'True')
-# while result.status < 4:
-#     print result.status
-#     time.sleep(0.25)
-# print "Execution Finished"
-# arcpy.CopyFeatures_management(result.getOutput(0),
-#                               r'F:\DonMonteith\Watersheds\AWMBWatersheds.shp')
-# arcpy.CopyFeatures_management(result.getOutput(1),
-#                               r'F:\DonMonteith\Watersheds\AWMBPourPoints.shp')
+# Generate watersheds using AGOL Watersheds hydrology geoprocessing service
+arcpy.ImportToolbox('http://hydro.arcgis.com/arcgis/services;Tools/Hydrology;<username>;<password>', 'hydro')
+result = arcpy.Watershed_hydro(r'F:\DonMonteith\Watersheds\AWMN.shp',
+                               'FID',
+                               '',
+                               'Meters',
+                               'FINEST',
+                               'False',
+                               'True')
+while result.status < 4:
+    print result.status
+    time.sleep(0.25)
+print "Execution Finished"
+arcpy.CopyFeatures_management(result.getOutput(0),
+                              r'F:\DonMonteith\Watersheds\AWMBWatersheds.shp')
+arcpy.CopyFeatures_management(result.getOutput(1),
+                              r'F:\DonMonteith\Watersheds\AWMBPourPoints.shp')
 
 
 # Summarise elevation in watersheds usign AGOL SummarizeElevation elevation geoprocessing service
