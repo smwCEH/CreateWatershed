3fddd727fc00f1074cea19583c954b27373eac63
Simon Wright <smw@ceh.ac.uk>
2015-11-24 15:45:53 +0000

    Added Desktop watershed script Added code to calculate slope and aspect and create Zonal Statistics tables summarising elevation, slope and aspect in the calculated watershed.

--- DesktopWatershed.py
+++ DesktopWatershed.py
@@ -8,7 +8,7 @@ from arcpy.sa import *
 arcpy.CheckOutExtension('Spatial')
 
 
-ROOTFOLDER = r'E:\smw\Workspace\QGIS'
+ROOTFOLDER = r'F:\DonMonteith\Watersheds\ArcGISDesktopWatersheds'
 
 
 ArcGISFolder = r'ArcGIS'
@@ -23,7 +23,10 @@ if not arcpy.Exists(FGDB):
     arcpy.CreateFileGDB_management(os.path.split(FGDB)[0], os.path.split(FGDB)[1], 'CURRENT')
 
 
-inDTM = r'E:\smw\Workspace\QGIS\srtm\n54_w003_1arc_v3.tif'
+# inDTM = r'E:\smw\Workspace\QGIS\srtm\n54_w003_1arc_v3.tif'
+# inDTM = r'E:\smw\Workspace\QGIS\ArcGIS\SRTM.gdb\n54_w003_1arc_v3'
+inDTM = r'F:\DonMonteith\Watersheds\ArcGISDesktopWatersheds\ArcGIS\SRTM.gdb\n54_w003_1arc_v3'
+print('\n\ninDTM:\t{0}\n\n'.format(inDTM))
 
 
 env.workspace = FGDB
@@ -42,6 +45,15 @@ def raster_function(raster):
         print('\tDeleting {0}...'.format(outRaster))
         arcpy.Delete_management(outRaster)
     return outRaster
+
+
+def table_function(table):
+    outTable = os.path.join(FGDB, table)
+    outTable = os.path.normpath(outTable)
+    if arcpy.Exists(outTable):
+        print('\tDeleting {0}...'.format(outTable))
+        arcpy.Delete_management(outTable)
+    return outTable
 
 
 n = 1
@@ -111,7 +123,8 @@ outFlowAccumulation.save(fgdbRaster)
 n += 1
 print('{0}'.format(str(n).zfill(2)))
 fgdbRaster = raster_function('a' + str(n).zfill(2) + '_Con')
-outCon = Con(outFlowAccumulation, 1, None, 'Value = 415880')
+# outCon = Con(outFlowAccumulation, 1, None, 'Value = 415880')
+outCon = Con(outFlowAccumulation, 1, None, 'Value = 298450')
 print('\tSaving {0}...'.format(fgdbRaster))
 outCon.save(fgdbRaster)
 
@@ -122,3 +135,40 @@ fgdbRaster = raster_function('a' + str(n).zfill(2) + '_Watershed')
 outWatershed = Watershed(outFlowDirection, outCon, 'Value')
 print('\tSaving {0}...'.format(fgdbRaster))
 outWatershed.save(fgdbRaster)
+
+
+n += 1
+print('{0}'.format(str(n).zfill(2)))
+fgdbRaster = raster_function('a' + str(n).zfill(2) + '_Slope')
+outSlope = Slope(outFill, 'DEGREE', 1.0)
+print('\tSaving {0}...'.format(fgdbRaster))
+outSlope.save(fgdbRaster)
+
+
+n += 1
+print('{0}'.format(str(n).zfill(2)))
+fgdbRaster = raster_function('a' + str(n).zfill(2) + '_Aspect')
+outAspect = Aspect(outFill)
+print('\tSaving {0}...'.format(fgdbRaster))
+outAspect.save(fgdbRaster)
+
+
+n += 1
+print('{0}'.format(str(n).zfill(2)))
+fgdbTable = table_function('a' + str(n).zfill(2) + '_ZS_Elevation')
+outZonalStatistics = ZonalStatisticsAsTable(outWatershed, 'Value', outFill, fgdbTable, 'DATA', 'ALL')
+print('\tCreating {0}...'.format(fgdbTable))
+
+
+n += 1
+print('{0}'.format(str(n).zfill(2)))
+fgdbTable = table_function('a' + str(n).zfill(2) + '_ZS_Slope')
+outZonalStatistics = ZonalStatisticsAsTable(outWatershed, 'Value', outSlope, fgdbTable, 'DATA', 'ALL')
+print('\tCreating {0}...'.format(fgdbTable))
+
+
+n += 1
+print('{0}'.format(str(n).zfill(2)))
+fgdbTable = table_function('a' + str(n).zfill(2) + '_ZS_Aspect')
+outZonalStatistics = ZonalStatisticsAsTable(outWatershed, 'Value', outAspect, fgdbTable, 'DATA', 'MEAN')
+print('\tCreating {0}...'.format(fgdbTable))
